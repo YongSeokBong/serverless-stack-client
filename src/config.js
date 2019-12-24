b1f70c1d3322a4d778b1874447a011cf0e73db98
YongSeokBong <ysb412@naver.com>
2019-12-24 10:38:56 +0900

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-northeast-2",
+      BUCKET: "notes-linkor-uploads"
+    },
+    apiGateway: {
+      REGION: "ap-northeast-2",
+      URL: "https://isiip32qg7.execute-api.ap-northeast-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-northeast-2",
+      USER_POOL_ID: "ap-northeast-2_OG0qr5PfL",
+      APP_CLIENT_ID: "4p545gfgm8ltt4tom742g7h9j0",
+      IDENTITY_POOL_ID: "ap-northeast-2:527c3af9-f72d-4cc4-9f19-02a5a9b1b4e4 "
+    }
+  };

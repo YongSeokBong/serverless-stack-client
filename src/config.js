8582f78dc37fa0f45c98ac323f67f4aeaf906dad
YongSeokBong <ysb412@naver.com>
2019-12-31 13:12:59 +0900

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
-export default {
+
+  const dev = {
+    STRIPE_KEY: "pk_test_2WIQLch6p18yqzMhZzb4J3LD007sh2uIj4",
     s3: {
       REGION: "ap-northeast-2",
-      BUCKET: "notes-linkor-uploads"
+      BUCKET: "notes-linkor"
     },
     apiGateway: {
       REGION: "ap-northeast-2",
@@ -10,7 +12,36 @@ export default {
     cognito: {
       REGION: "ap-northeast-2",
       USER_POOL_ID: "ap-northeast-2_OG0qr5PfL",
-      APP_CLIENT_ID: "4p545gfgm8ltt4tom742g7h9j0",
-      IDENTITY_POOL_ID: "ap-northeast-2:527c3af9-f72d-4cc4-9f19-02a5a9b1b4e4 "
+      APP_CLIENT_ID: "5un743cc8c98jt5v5dqv4jpiqf",
+      IDENTITY_POOL_ID: "ap-northeast-2:27a3305b-8ca7-47c7-9703-b28934de3255"
     }
+  };
+  
+  const prod = {
+    STRIPE_KEY: "pk_test_2WIQLch6p18yqzMhZzb4J3LD007sh2uIj4",
+    s3: {
+      REGION: "ap-northeast-2",
+      BUCKET: "notes-linkor"
+    },
+    apiGateway: {
+      REGION: "ap-northeast-2",
+      URL: "https://isiip32qg7.execute-api.ap-northeast-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-northeast-2",
+      USER_POOL_ID: "ap-northeast-2_OG0qr5PfL",
+      APP_CLIENT_ID: "5un743cc8c98jt5v5dqv4jpiqf",
+      IDENTITY_POOL_ID: "ap-northeast-2:27a3305b-8ca7-47c7-9703-b28934de3255"
+    }
+  };
+  
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+  
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
   };

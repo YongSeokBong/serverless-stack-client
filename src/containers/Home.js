aa49ebbdf1800ad94e1243cc190c8cf700dfa33d
YongSeokBong <ysb412@naver.com>
2020-01-02 14:59:20 +0900

    logo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -54,7 +54,7 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
+        <h1>LINKOR</h1>
         <p>A simple note taking app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
